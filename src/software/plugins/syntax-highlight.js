7c4ab4fc5bf4d7946b9e99f751760fe1c13c599d
Mayur Panchal <mayurpanchal002@gmail.com>
2020-10-10 15:01:14 +0530

    fixed wrong import statments

--- src/software/plugins/syntax-highlight.js
+++ src/software/plugins/syntax-highlight.js
@@ -1,4 +1,5 @@
-import { execSync } from 'child_process';
+const { execSync } = require('child_process');
+const chalk = require('chalk');
 
 const log = console.log;
 
@@ -6,11 +7,13 @@ module.exports = () => {
 	try {
 		// Syntax Highlighting
 		log(chalk.blueBright('Installing Syntax Highlighting'));
+		execSync('cd ~');
 		execSync(
 			'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git'
 		);
+		log('project cloned');
 		execSync(
-			'echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc'
+			'echo "source ${(q-)HOME}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc'
 		);
 		execSync('source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh');
 		log(chalk.green('Successfully installed Syntax Highlighting'));
